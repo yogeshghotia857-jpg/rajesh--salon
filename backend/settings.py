64cbcb64fa6341eab66c454fd0332a91cc0728d5
Yogesh Ghotia <yogeshghotia857@gmail.com>
2026-09-20 16:37:45 +0530

    Configure WhiteNoise for static files

--- backend/settings.py
+++ backend/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     "django.middleware.security.SecurityMiddleware",
+    
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
 
     "django.contrib.sessions.middleware.SessionMiddleware",
 
